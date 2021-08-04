f21e3261b3dd63dbcbffe188f012cdd79ca12d79
Jonathan Daniel <gcagrad19@gmail.com>
2021-08-03 21:06:41 -0400

    Final Initial Submission

--- src/App.js
+++ src/App.js
@@ -89,33 +89,56 @@ function App() {
     }
 
     return (
-        <div className="bg-gray-100 grid grid-cols-2">
+        <div className="bg-gray-100 grid grid-cols-2 min-h-screen">
             <div>
-                <form>
-                    <label for="title">Title:</label>
-                    <input
-                        type="text"
-                        id="title"
-                        value={newTaskTitle}
-                        onChange={handleTitleChange}
-                    />
-                    <br />
-                    <label for="description">Description: </label>
-                    <input
-                        type="text"
-                        id="description"
-                        value={newTaskDescription}
-                        onChange={handleDescriptionChange}
-                    />
-                    <br />
-                    <input type="submit" onClick={handleNewTaskSubmit} />
-                </form>
-
-                <button onClick={handleShowUnfinishedTasksClick}>
-                    {showAllTasks
-                        ? "Click to show all unfinished tasks"
-                        : "Click to show all tasks"}
-                </button>
+                <div className="p-5">
+                    <div className="rounded-md bg-white shadow-lg p-5 flex flex-col">
+                        <h1 className="text-center text-2xl font-bold">
+                            Add New Tasks Here:
+                        </h1>
+                        <form className="flex flex-col">
+                            <div className="flex justify-end pt-2">
+                                <label for="title" className="p-2">
+                                    Title:
+                                </label>
+                                <input
+                                    className="bg-gray-300 rounded-lg p-2"
+                                    type="text"
+                                    id="title"
+                                    value={newTaskTitle}
+                                    onChange={handleTitleChange}
+                                />
+                            </div>
+                            <div className="flex justify-end py-2">
+                                <label for="description" className="p-2">
+                                    Description:{" "}
+                                </label>
+                                <input
+                                    className="bg-gray-300 rounded-lg p-2"
+                                    type="text"
+                                    id="description"
+                                    value={newTaskDescription}
+                                    onChange={handleDescriptionChange}
+                                />
+                            </div>
+                            <input
+                                type="submit"
+                                className="py-2 rounded-lg justify-self-center"
+                                onClick={handleNewTaskSubmit}
+                            />
+                        </form>
+                        <div className="flex flex-col py-3">
+                            <button
+                                onClick={handleShowUnfinishedTasksClick}
+                                className="py-2 rounded-lg justify-self-center bg-gray-200"
+                            >
+                                {showAllTasks
+                                    ? "Click to show all unfinished tasks"
+                                    : "Click to show all tasks"}
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div>{renderTasks()}</div>
         </div>
